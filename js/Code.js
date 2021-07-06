3eeec3fb70405e990f87d3210515e1373b14f73f
AtharvaRajpurkar <75119919+AtharvaRajpurkar@users.noreply.github.com>
2021-07-06 19:27:17 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+   
+    fill("white")
+    textSize(15)
+    text("U C N T I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a perticular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A E D T A A S B", 100,350)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,370)
+
+    
+
+}
